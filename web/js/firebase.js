0320dc941832026a16d05de4e50e78e4e82af065
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-16 07:45:36 -0800

    Update firebase.js

--- web/js/firebase.js
+++ web/js/firebase.js
@@ -1,1 +1,17 @@
-// Pega aqui tu firebaseConfig
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALrk15Qvqrq6zCVTxZ7U9wSnnZIqeSmv4",
+  authDomain: "novagrow-app.firebaseapp.com",
+  projectId: "novagrow-app",
+  storageBucket: "novagrow-app.firebasestorage.app",
+  messagingSenderId: "976275033149",
+  appId: "1:976275033149:web:e40c6510684bd06c82ae54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
