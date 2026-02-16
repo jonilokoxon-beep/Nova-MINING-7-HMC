1e385cb5d62acc377201a08f8aa8f4bfa9a5addb
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-16 11:37:52 -0800

    Update MainActivity.java

--- android/MainActivity.java
+++ android/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nova.mining;
+package com.novamining.hmc;
 
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
   @Override
-  protected void onCreate(Bundle b) {
-    super.onCreate(b);
-    WebView w = new WebView(this);
-    w.getSettings().setJavaScriptEnabled(true);
-    w.loadUrl("https://TU_GITHUB_PAGES/index.html");
-    setContentView(w);
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    WebView webView = new WebView(this);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.loadUrl("file:///android_asset/index.html");
+
+    setContentView(webView);
   }
 }
