239eb24cb3c734eaf7454da8c561e58be3d8c2d7
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-16 14:28:10 -0800

    Update auth.js

--- web/js/auth.js
+++ web/js/auth.js
@@ -1,4 +1,12 @@
-function login(){
-  alert("Login demo activo");
-  window.location.href = "dashboard.html";
-}
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const status = document.getElementById("status");
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => window.location.href = "dashboard.html")
+    .catch(e => status.innerText = e.message);
+};
