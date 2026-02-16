845e982d4264ee94c3f90ead24f9d0e737b02c24
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-16 11:49:12 -0800

    Update app.js

--- web/js/app.js
+++ web/js/app.js
@@ -3,13 +3,23 @@
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
 <script>
-  const firebaseConfig = {
-    apiKey: "PEGA_AQUI",
-    authDomain: "PEGA_AQUI",
-    projectId: "PEGA_AQUI",
-    storageBucket: "PEGA_AQUI",
-    messagingSenderId: "PEGA_AQUI",
-    appId: "PEGA_AQUI"
+  const firebaseConfig = {// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALrk15Qvqrq6zCVTxZ7U9wSnnZIqeSmv4",
+  authDomain: "novagrow-app.firebaseapp.com",
+  projectId: "novagrow-app",
+  storageBucket: "novagrow-app.firebasestorage.app",
+  messagingSenderId: "976275033149",
+  appId: "1:976275033149:web:e40c6510684bd06c82ae54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   };
 
   firebase.initializeApp(firebaseConfig);
