07de9483718b8fc6362ed3f1b8a0cc9750a70c45
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-16 14:46:15 -0800

    Delete web directory

--- web/js/admin.js
+++
@@ -1,9 +0,0 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const cont = document.getElementById("users");
-
-const q = await getDocs(collection(db,"users"));
-q.forEach(doc => {
-  cont.innerHTML += `<p>${doc.id} → ${JSON.stringify(doc.data())}</p>`;
-});
