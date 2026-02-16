fc6cbaf3ab626d9a54175bffde1438084fd1bfea
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-16 11:47:55 -0800

    Update app.js

--- web/js/app.js
+++ web/js/app.js
@@ -1,3 +1,22 @@
+<!-- Firebase -->
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+<script>
+  const firebaseConfig = {
+    apiKey: "PEGA_AQUI",
+    authDomain: "PEGA_AQUI",
+    projectId: "PEGA_AQUI",
+    storageBucket: "PEGA_AQUI",
+    messagingSenderId: "PEGA_AQUI",
+    appId: "PEGA_AQUI"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+</script>
+
 import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
