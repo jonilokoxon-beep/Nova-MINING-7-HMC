3498e833d6280715693ec988ed00a9e17aea54e3
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-17 16:00:52 -0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,4 +1,24 @@
-document.getElementById("content").innerHTML = `
-<h2>Productos</h2>
-<p>Lista de productos para comprar</p>
-`;
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export async function loadProducts() {
+  const div = document.getElementById("plans");
+  if (!div) return;
+
+  div.innerHTML = "Cargando productos...";
+
+  const snap = await getDocs(collection(db, "products"));
+
+  div.innerHTML = "";
+  snap.forEach(d => {
+    const p = d.data();
+    if (!p.active) return;
+
+    div.innerHTML += `
+      <div class="plan">
+        <h4>${p.name}</h4>
+        <p>$${p.price}</p>
+      </div>
+    `;
+  });
+}
