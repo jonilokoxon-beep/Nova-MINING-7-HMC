1755e1e493ea5e1e6d56c5eb4a15a64cb3eb588b
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-19 14:10:19 -0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,8 @@
-// js/profile.js
-
 import { auth, db } from "./firebase.js";
 import {
   doc,
   getDoc,
-  collection,
-  query,
-  where,
-  getDocs
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 export async function loadProfile() {
@@ -17,33 +12,20 @@ export async function loadProfile() {
   const snap = await getDoc(doc(db, "users", user.uid));
   const data = snap.data();
 
-  document.getElementById("p-id").innerText = user.uid.slice(0, 6);
-  document.getElementById("p-balance").innerText =
+  document.getElementById("profile-email").innerText = user.email;
+  document.getElementById("profile-balance").innerText =
     Number(data.balance || 0).toFixed(2);
-
-  loadTransactions(user.uid);
 }
 
-async function loadTransactions(uid) {
-  const div = document.getElementById("transactionHistory");
+window.updateProfile = async function () {
+  const user = auth.currentUser;
+  if (!user) return;
 
-  const q = query(
-    collection(db, "transactions"),
-    where("uid", "==", uid)
-  );
+  const newName = document.getElementById("profile-name").value;
 
-  const snap = await getDocs(q);
-
-  div.innerHTML = "";
-
-  snap.forEach(docSnap => {
-    const t = docSnap.data();
-
-    div.innerHTML += `
-      <div class="tx-item">
-        <span>${t.type}</span>
-        <b>$${t.amount}</b>
-      </div>
-    `;
+  await updateDoc(doc(db, "users", user.uid), {
+    name: newName
   });
-}
+
+  alert("Perfil actualizado correctamente");
+};
