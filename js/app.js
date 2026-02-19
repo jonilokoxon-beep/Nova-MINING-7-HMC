6c0896a2b44c6a4ae7853acfaf1b95976aea7e38
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-19 14:10:54 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,8 @@
-import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-import { loadProducts } from "./products.js";
-import { loadOrders } from "./orders.js";
-import { loadProfile } from "./profile.js";
-
 window.go = function (id) {
-  document.querySelectorAll(".page").forEach(p => p.style.display = "none");
-  document.getElementById(id).style.display = "block";
+  document.querySelectorAll(".page").forEach(p => {
+    p.style.display = "none";
+  });
+
+  const page = document.getElementById(id);
+  if (page) page.style.display = "block";
 };
-
-onAuthStateChanged(auth, user => {
-  if (!user) {
-    window.location.href = "login.html";
-    return;
-  }
-
-  go("home");
-
-  loadProducts();
-  loadOrders();
-  loadProfile();
-});
