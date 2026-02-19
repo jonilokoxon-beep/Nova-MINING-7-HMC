5e6c8a43ccfeafabdc9474b51b8ab9750f76ec21
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-19 13:37:50 -0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,9 @@
+// js/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
+import { getFirestore } from "https://www.
+  
 const firebaseConfig = {
   apiKey: "AIzaSyALrk15Qvqrq6zCVTxZ7U9wSnnZIqeSmv4",
   authDomain: "novagrow-app.firebaseapp.com",
