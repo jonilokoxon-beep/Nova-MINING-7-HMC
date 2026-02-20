3bc52e8cd45852dd6f6008d7ada9b9d9924fab5d
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-20 08:48:50 -0800

    Update reglas.js

--- firestore/reglas.js
+++ firestore/reglas.js
@@ -11,13 +11,25 @@ service cloud.firestore {
       allow write: if request.auth.token.email == "joni.lokoxon@gmail.com";
     }
 
-    match /vip/{doc} {
-      allow read: if true;
-      allow write: if request.auth.token.email == "joni.lokoxon@gmail.com";
-    }
-
     match /orders/{doc} {
       allow read, write: if request.auth != null;
+    }
+
+    match /deposits/{doc} {
+      allow read, write: if request.auth != null;
+    }
+
+    match /withdrawals/{doc} {
+      allow read, write: if request.auth != null;
+    }
+
+    match /transactions/{doc} {
+      allow read, write: if request.auth != null;
+    }
+
+    match /rescueCodes/{doc} {
+      allow read: if request.auth != null;
+      allow write: if request.auth.token.email == "joni.lokoxon@gmail.com";
     }
   }
 }
