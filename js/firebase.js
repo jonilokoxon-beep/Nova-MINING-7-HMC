8ab7b8b4bdf814477c0e1f8f9be5adaf21282231
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-20 05:47:47 -0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,3 @@
-// js/firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -17,4 +15,3 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
