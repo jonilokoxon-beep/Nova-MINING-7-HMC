2844b7638d6a6752e026b9791114f56b8f9e6fd6
jonilokoxon-beep <joni.lokoxon@gmail.com>
2026-02-20 09:30:47 -0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -11,18 +11,6 @@ import {
   setDoc 
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// =============================
-// CONFIG FIREBASE (TUYA)
-// =============================
-const firebaseConfig = {
-  apiKey: "AIzaSyALrk15Qvqrq6zCVTxZ7U9wSnnZIqeSmv4",
-  authDomain: "novagrow-app.firebaseapp.com",
-  projectId: "novagrow-app",
-  storageBucket: "novagrow-app.appspot.com",
-  messagingSenderId: "976275033149",
-  appId: "1:976275033149:web:e40c6510684bd06c82ae54"
-};
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
